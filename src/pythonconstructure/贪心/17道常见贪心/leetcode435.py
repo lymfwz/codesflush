99a3f6368ca3565e96577595edeb6b98bcdb8f80
lymfwz <207194494@qq.com>
2025-03-29 23:41:09 +0800

    贪心

--- src/pythonconstructure/贪心/17道常见贪心/leetcode435.py
+++ src/pythonconstructure/贪心/17道常见贪心/leetcode435.py
@@ -2,27 +2,51 @@
 # 返回 需要移除区间的最小数量，使剩余区间互不重叠 。
 #
 # 注意 只在一点上接触的区间是 不重叠的。例如 [1, 2] 和 [2, 3] 是不重叠的。
+from math import inf
 from typing import List
 
 
 class Solution:
     def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
         # 排序
-        intervals.sort(key=lambda x: (x[0], x[1]))
+        intervals.sort(key=lambda x: x[0])
         n = len(intervals)
         left = intervals[0][0]
         right = intervals[0][1]
         count = 0
-        for i in range(1, n):
-            if intervals[i][0] < right:
-                left = max(intervals[i][0], left)
-                right = min(intervals[i][1], right)
+        for l, r in intervals:
+            if l < right:
+                left = max(l, left)
+                right = min(r, right)
                 count += 1
             else:
-                left = intervals[i][0]
-                right = intervals[i][1]
+                left = l
+                right = r
         return count
+
+    def eraseOverlapIntervals2(self, intervals: List[List[int]]) -> int:
+        intervals = sorted(intervals, key=lambda x: x[1])  # 按区间右端点排序
+        ans = 0
+        pre_r = -inf  # 初始化前一个区间的右端点为负无穷
+        for l, r in intervals:
+            if l >= pre_r:  # 如果当前区间的左端点 >= 前一个区间的右端点，说明不重叠
+                ans += 1
+                pre_r = r
+        return len(intervals) - ans  # 需要移除的区间数 = 总区间数 - 不重叠的区间数
+
+    def eraseOverlapIntervals3(intervals: List[List[int]]) -> int:
+        intervals.sort(key=lambda x: x[0])  # 按左端点排序
+        n = len(intervals)
+        right = -inf  # 初始化前一个区间的右端点为负无穷
+        count = 0  # 记录不重叠的区间数
+        for l, r in intervals:
+            if l >= right:  # 不重叠
+                count += 1
+                right = r
+            else:  # 重叠时，选择右端点较小的区间
+                right = min(right, r)
+        return n - count  # 需要移除的区间数
 
 if __name__ == '__main__':
     s = Solution()
-    s.eraseOverlapIntervals([[-52,31],[-73,-26],[82,97],[-65,-11],[-62,-49],[95,99],[58,95],[-31,49],[66,98],[-63,2],[30,47],[-40,-26]])
+    s.eraseOverlapIntervals3([[0,2],[1,3],[2,4],[3,5],[4,6]])
